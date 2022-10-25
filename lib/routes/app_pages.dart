8875b8d9f3bbb6d3d6b0fc258144fc3e1f47d0b5
jmbscifo <josemanuelbarbacifo@gmail.com>
2022-10-25 12:49:23 +0200

    prepare inital strucure

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,12 @@
+import 'package:films_app_flutter/pages/home_page.dart';
+import 'package:films_app_flutter/routes/app_routes.dart';
+import 'package:get/get.dart';
+
+class AppPages {
+  static final routes = [
+    GetPage(
+      name: Routes.HOME,
+      page: () => HomePage(),
+    )
+  ];
+}
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,7 @@
+class Routes {
+  static const HOME = _Paths.HOME;
+}
+
+class _Paths {
+  static const HOME = '/home';
+}
